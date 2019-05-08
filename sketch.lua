95f33d2c5837c24c3be4e6527805dbe174a3cb5f
marked <marked@localhost.localdomain>
2019-05-08 10:48:53 -0500

    Statistics on response codes

--- sketch.lua
+++ sketch.lua
@@ -1,6 +1,4 @@
 dofile("table_show.lua")
-dofile("urlcode.lua")
-JSON = (loadfile "JSON.lua")()
 
 local item_type = os.getenv('item_type')
 local item_value = os.getenv('item_value')
@@ -8,19 +6,17 @@ local item_dir = os.getenv('item_dir')
 local warc_file_base = os.getenv('warc_file_base')
 
 local url_count = 0
-local tries = 0
 local downloaded = {}
-local addedtolist = {}
 local abortgrab = false
-
-local posts = {}
-local requested_children = {}
-local outlinks = {}
+local code_counts = {}
 
 for ignore in io.open("ignore-list", "r"):lines() do
   downloaded[ignore] = true
 end
 
+local resp_codes_file = io.open(item_dir..'/'..warc_file_base..'_data.txt', 'w')
+
+-----------------------------------------------------------------------------------------------------------------------
 
 wget.callbacks.httploop_result = function(url, err, http_stat)
   status_code = http_stat["statcode"]
@@ -29,28 +25,34 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
   io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. "  \n")
   io.stdout:flush()
 
-  -- storage.sketch
-  if (string.match(url["host"], "^storage\.sketch\.sonymobile\.com") and status_code == 307) then
+  if code_counts[status_code] == nil then
+    code_counts[status_code] = 1
+  else
+    code_counts[status_code] = code_counts[status_code] + 1
+  end
+
+  -- Expected results
+  if (string.match(url["host"], "^storage%.sketch%.sonymobile%.com") and status_code == 307) or 
+     (string.match(url["host"], "^storage%.sketch%.sonymobile%.com") and status_code == 404) or
+     (string.match(url["host"], "^sketch[-]cloud[-]storage%.s3%.amazonaws%.com") and status_code == 200) then
+    resp_codes_file:write(url["url"] .. ":" .. status_code .. "\n")
     return wget.actions.NOTHING
   end
 
-  -- storage.sketch
-  if (string.match(url["host"], "^storage\.sketch\.sonymobile\.com") and status_code == 404) then
-    return wget.actions.EXIT
-  end
-
-  -- AWS S3
-  if (string.match(url["host"], "^sketch[-]cloud[-]storage\.s3\.amazonaws\.com") and status_code == 200) then
-    return wget.actions.EXIT
-  end
-
+  -- Unexpected results
   abortgrab = true
   return wget.actions.ABORT
 end
 
+-----------------------------------------------------------------------------------------------------------------------
+
 wget.callbacks.before_exit = function(exit_status, exit_status_string)
+  resp_codes_file_file:close()
+  table.show(code_counts)
   if abortgrab == true then
     return wget.exits.IO_FAIL
   end
   return exit_status
 end
+
+-----------------------------------------------------------------------------------------------------------------------
